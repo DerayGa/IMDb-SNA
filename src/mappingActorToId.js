0eeff969708220b0e4a487ecc465546994a6f25e
Deray <deraygo@gmail.com>
2016-10-08 22:01:32 +0800

    support strange name character, parse raw html from m.imdb

--- src/mappingActorToId.js
+++ src/mappingActorToId.js
@@ -38,51 +38,82 @@ const getIdByName = (actor) => {
     return;
   }
   if (!isAlphaOrParen(actor.name)) {
-    console.log('skip not english name for now', actor.name);
-    count--;
-    checkCompleted();
-    return;
+    console.log('get from m.imdb');
+
+    var url = `http://m.imdb.com/find?q=${encodeURI(actor.name)}`;
+
+    fetch(url)
+      .then((res) => (
+        res.text()
+      )).then((body) => {
+        jsdom.env(
+          body,
+          ["http://code.jquery.com/jquery.js"],
+          (err, window) => {
+            const $ = window.$
+            const result = $('section.posters > div.poster > div.label > div.title > a');
+            result.each((index, node) => {
+              if($(node).text() == actor.name){
+                actor.id = $(node).attr('href').split('/')[2];
+                return false;
+              }
+            });
+
+            if (actor.id) {
+              console.log('found! ', actor.name, actor.id);
+              
+              saveToJSON();
+            } else {
+              console.log('NOT found! ', actor.name);
+            }
+            count--;
+
+            checkCompleted();
+          }
+        )
+      });
+  } else {
+    console.log('get from imdb api');
+    var url = `http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=${actor.name}`;
+
+    fetch(url)
+      .then((res) => (
+        res.json()
+      )).then((json) => {
+        if (json.name_exact) {
+          json.name_exact.forEach((obj) => {
+            if (obj.name == actor.name) {
+              actor.id = obj.id;
+            }
+          });
+        }
+        if (json.name_approx && !actor.id) {
+          json.name_approx.forEach((obj) => {
+            if (obj.name == actor.name) {
+              actor.id = obj.id;
+            }
+          });
+        }
+        if (json.name_popular && !actor.id) {
+          json.name_popular.forEach((obj) => {
+            if (obj.name == actor.name) {
+              actor.id = obj.id;
+            }
+          });
+        }
+
+        if (actor.id) {
+          console.log('found! ', actor.name, actor.id);
+          
+          saveToJSON();
+        } else {
+          console.log('NOT found! ', actor.name);
+        }
+        count--;
+
+        checkCompleted();
+      });
   }
-  //const url = `http://www.imdb.com/find?q=${actor.name}&s=nm`;
-  //const url = `http://m.imdb.com/find?q=${actor.name}`;
-  const url = `http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=${actor.name}`;
-  fetch(url)
-    .then((res) => (
-      res.json()
-    )).then((json) => {
-      if (json.name_exact) {
-        json.name_exact.forEach((obj) => {
-          if (obj.name == actor.name) {
-            actor.id = obj.id;
-          }
-        });
-      }
-      if (json.name_approx && !actor.id) {
-        json.name_approx.forEach((obj) => {
-          if (obj.name == actor.name) {
-            actor.id = obj.id;
-          }
-        });
-      }
-      if (json.name_popular && !actor.id) {
-        json.name_popular.forEach((obj) => {
-          if (obj.name == actor.name) {
-            actor.id = obj.id;
-          }
-        });
-      }
-
-      if (actor.id) {
-        console.log('found! ', actor.name, actor.id);
-        
-        saveToJSON();
-      } else {
-        console.log('NOT found! ', actor.name);
-      }
-      count--;
-
-      checkCompleted();
-    });
 };
 
 
